76aa00fb0117c3f69de13fc9068e3cfb303988a1
Vaibhav Mishra <vmishra175@gmail.com>
2021-08-15 11:22:38 +0530

    Richest Customer Wealth

---
+++ RichestCustomerWealth.java
@@ -0,0 +1,49 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int maxw = 0;
+        for (int[] account: accounts) {
+            int sum = 0;
+            for (int acc: account) {
+                sum += acc;
+            }
+            maxw = Math.max(maxw, sum);
+        }
+        return maxw;
+    }
+}
+
+/*
+    T: O(mn)
+    S: O(1)
+    Variation 1: Using loops
+
+    public int maximumWealth(int[][] accounts) {
+        int max = 0;
+        for (int[] customer : accounts) {
+            int wealth = 0;
+            for (int account : customer)
+                wealth += account;
+            max = Math.max(max, wealth);
+        }
+        return max;
+    }
+    Variation 2: Using Loop and Stream
+
+    public int maximumWealth(int[][] accounts) {
+        int max = 0;
+        for (int[] customer : accounts) {
+            int wealth = Arrays.stream(customer).sum();
+            max = Math.max(max, wealth);
+        }
+        return max;
+    }
+    Variation 3: Using Streams
+
+    public int maximumWealth(int[][] accounts) {
+        return Arrays.stream(accounts)
+                    .mapToInt(customerAccount -> Arrays.stream(customerAccount).sum())
+                    .max()
+                    .getAsInt();
+    }
+
+    */
